52373ffca85a4a1b8810184bb198924b99357c88
Etienne Grosdidier <141655132+egrosdidier@users.noreply.github.com>
2025-02-08 18:38:33 +0100

    Update OrdoStup.py

--- OrdoStup.py
+++ OrdoStup.py
@@ -81,6 +81,8 @@ if st.button("Générer l'ordonnance PDF"):
         logo_path = "logo_structure.png"
         with open(logo_path, "wb") as f:
             if preferences["logo"] is not None:
+            with open(logo_path, "wb") as f:
+                f.write(preferences["logo"].read())
             f.write(preferences["logo"].read())
     else:
         from PIL import Image
